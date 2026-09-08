58bee1edb0280eb6c0862c1519c0bc54a6d87770
Behruz <bitcrown82@gmail.com>
2026-09-08 17:05:11 +0500

    bu  kun biz botga menu va kp narlsala qoshgdik

--- bot.py
+++ bot.py
@@ -1,13 +1,54 @@
 from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes, Application
+from telegram.ext import Application,  CommandHandler, ContextTypes
 import env
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+   print(update.effective_user)
+
+
+
+   first_name = update.effective_user.first_name
+   last_name = update.effective_user.last_name
+   user_id = update.effective_user.id
+   is_bot = update.effective_user.is_bot
+   language_code = update.effective_user.language_code
+   username = update.effective_user.username
+
+   if last_name is None:
+      last_name = ""
+
+   if is_bot == True:
+      is_bot = "Bot ekansiz"
+   else:
+      is_bot = "Bot emasiz"
+
+   if username == None:
+      username = "Username yo'q"
+
+
+
+   reply_text = f"""
+            Salom👋 {first_name} {last_name}.
+
+            Sizning ma'lumotlaringiz menda bor:
+            1. Telegram ID: __{user_id}__
+            2. username : @{username}
+            3. Telegramni {language_code}
+            4. {is_bot}.
+      """
+
+
+   await update.message.reply_text(reply_text)
+
+async def cheksiz_salom(update: Update, context: ContextTypes.DEFAULT_TYPE):
+   while True:
+      reply_text = update.message.reply_text("Salom👋!")
+      await reply_text
 
 app = Application.builder().token(env.BOT_TOKEN).build()
 
-def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-   print(update)
-   return update.message.reply_text(f"Salom {update.effective_user.first_name}")
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("cheksiz_salom", cheksiz_salom))
 
-
-app.add_handler(CommandHandler("ger_lst", start))
+print("Bot ishga tushdi!")
 app.run_polling()
